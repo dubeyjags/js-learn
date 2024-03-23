c407258ef4054b669e250962feb7616e49b7cf62
Jags dubey <dubeyjags@gmail.com>
2024-03-23 05:52:39 +0000

    var files

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const employeeId = 2323232
+let employeeName = "Jagdamba"
+var employeeEmail = "jd@iskool.com"
+city = "noida" 
+
+//var cannot be used because it doesn't identify the scope 
+
+console.table([employeeId, employeeEmail, employeeName, city]);
