360334e1bc123934e19b17aa7810db2970ce75f2
Jags dubey <dubeyjags@gmail.com>
2024-03-30 13:05:38 +0000

    changes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+//console.log(9999);
+//console.log(typeof (999));
+
+//datatype 
+// number => 2 power 53 or big init
+// string =  ""
+// boolea = true/false
+// null = nothing
+// undefined = not defined or assinged the value
+// symbol = uniqueness
+//Object 
+
+// typeof get the type of the value data
+
+console.log(typeof null);
+console.log(typeof undefined);
+
