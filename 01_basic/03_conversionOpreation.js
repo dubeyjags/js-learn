9b8a9da40d71ccb8ec6fd75c3645197179969e42
Jags dubey <dubeyjags@gmail.com>
2024-03-30 17:03:04 +0000

    conversion oprations

---
+++ 01_basic/03_conversionOpreation.js
@@ -0,0 +1,13 @@
+let name = "Jags";
+let age = '33';
+
+let newNumber = Number(age);
+//console.log(typeof newNumber);
+
+let loggedIn = 1;
+// console.log(loggedIn);
+// console.log(typeof(loggedIn));
+// //let bLog = Boolean(loggedIn);
+// console.log(bLog);
+
+console.log(typeof(null));
