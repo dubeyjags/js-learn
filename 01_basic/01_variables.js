11e778106b8617aee71bb3e79cc9691370f52053
Jags dubey <dubeyjags@gmail.com>
2024-04-25 05:14:53 +0000

    notes at bottom

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,7 +2,9 @@ const employeeId = 2323232
 let employeeName = "Jagdamba"
 var employeeEmail = "jd@iskool.com"
 city = "noida" 
-
-//var cannot be used because it doesn't identify the scope 
+ 
 
 console.table([employeeId, employeeEmail, employeeName, city]);
+
+
+//var cannot be used because it doesn't identify the scope
