3277ad02bc3bb0e39a7ffdbafdbd5b4cbd3b9c15
Jags dubey <dubeyjags@gmail.com>
2024-04-28 07:08:21 +0000

    Strings

---
+++ 01_basic/05_String.js
@@ -0,0 +1,23 @@
+ const name = "Jags";
+ const count = 20;
+
+//  console.log(name + count + " value");
+//  console.log(`${name} ${count} " value"`);
+
+const gameName = new String("Jags");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+// console.log(gameName.substring(0,2));
+// console.log(gameName.slice(-1,4));
+const newStringOne = "     this is new       string";
+// console.log(newStringOne.trim());
+//console.log(newStringOne.includes('new'));
+//console.log(newStringOne.replace(' ','-'));
+//console.log(newStringOne.replace(' ',''));
+//console.log(newStringOne.split(''));
+
