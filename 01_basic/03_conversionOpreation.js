d0084549887ac017ce741cc5e8514eb75ef26095
Jags dubey <dubeyjags@gmail.com>
2024-04-28 04:59:51 +0000

    operations

--- 01_basic/03_conversionOpreation.js
+++ 01_basic/03_conversionOpreation.js
@@ -14,12 +14,38 @@ let loggedIn = 1;
 let a= 'bc1';
 //console.log(typeof 1); //Number
 //console.log(typeof '1'); //String
-console.log(Number(a)); //String
+//console.log(Number("ab1")); //String
 
 //text to number Number 
 //"1" = Number("1") => Number =1
 //"1abc" = Number("1abc") => NaN
 
 
+//------------------------Operations------------------------
 
+let value = 1;
+let neg= -value;
+//console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(3%5);
+
+let str = "jags";
+let str1 = " Dubey";
+//console.log(str + str1);
+
+// console.log("2" + 1);
+// console.log("2" + "1");
+// console.log(2 + 1);
+// console.log("2" + 1 + 1); // started from left or which comes first
+// console.log(2 + 1 + "1");
+// console.log(+true); // 1
+// console.log(true+); // Err
+
+let counter = 100;
+console.log(++counter);
+console.log(counter++);
 
