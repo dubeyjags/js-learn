d6608664070af6ffcb8dfbcc6dfd5ff73373b3de
Jags dubey <dubeyjags@gmail.com>
2024-05-01 03:15:04 +0000

    Arrays

---
+++ 02_basic/02_array.js
@@ -0,0 +1,33 @@
+const ramayan = ['Ram','Siya','Hanuman'];
+const Mahabharat = ['Krishna','Radha','Arjun'];
+
+// ramayan.push(Mahabharat); // arr push arr create new value with arr
+// console.log(ramayan);
+// console.log(ramayan[3][1]);
+
+
+// const history = ramayan.concat(Mahabharat); // concat returns the new arr with value
+// console.log(history);
+
+// const topIndian = [...Mahabharat, ...ramayan]; // concat returns the new arr with value
+// console.log(topIndian);
+
+// const newArr = [2,3,4,[4,5,5,[7,6,7,6,7]]];
+// const flatArr = newArr.flat(Infinity);
+// console.log(flatArr);
+
+console.log(Array.isArray('Jagdamba'));
+console.log(Array.from('Jagdamba'));
+console.log(Array.from({name:'Jagdamba'}));
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+console.log(Array.of(score1,score2,score3,));
+
+
+
+
+
+
+
